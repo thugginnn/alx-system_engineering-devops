308c01d36e16e36ceb37482e9fc30a35d8f1208b
thugginnn <paul.iyiade4@gmail.com>
2024-01-24 02:02:24 -0800

    0-create_a_file.pp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,4 @@
-# Puppet manifest to create a file in /tmp
-# File path is /tmp/school
-# File permission is 0744
-# File owner is www-data
-# File group is www-data
-# File contains "I love Puppet"
+#Creates a file in /tmp
 
 file { '/tmp/school':
   ensure   => file,
